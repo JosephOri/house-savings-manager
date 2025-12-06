6539ca16d4f982f54a69e30f320dc5bb4e3296e2
josephOri <ori100no@gmail.com>
2025-12-06 20:26:07 +0200

    created expense and income entity

--- src/household/financial-order/entities/financial-order.entity.ts
+++ src/household/financial-order/entities/financial-order.entity.ts
@@ -1,1 +1,28 @@
-export class FinancialOrder {}
+import { IsDate, IsNumber, IsOptional, IsString } from 'class-validator';
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export abstract class FinancialOrder {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  @IsString()
+  name: string;
+
+  @Column()
+  @IsNumber()
+  amount: number;
+
+  @Column()
+  @IsDate()
+  date: Date;
+
+  @Column()
+  @IsString()
+  category: string;
+
+  @Column()
+  @IsString()
+  @IsOptional()
+  description?: string;
+}
