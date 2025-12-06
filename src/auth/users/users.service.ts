2d5cd35b774e32f37f8d55b70a87b561e13dd32e
josephOri <ori100no@gmail.com>
2025-12-06 21:19:06 +0200

    updated Users.service

--- src/auth/users/users.service.ts
+++ src/auth/users/users.service.ts
@@ -2,15 +2,17 @@ import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from '@app/common';
+import { AbstractCrudService, User } from '@app/common';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class UsersService {
+export class UsersService extends AbstractCrudService<User> {
   constructor(
     @InjectRepository(User) private readonly userRepository: Repository<User>,
-  ) {}
+  ) {
+    super(userRepository);
+  }
 
   private async validateCreateUserDto(createUserDto: CreateUserDto) {
     const user = await this.userRepository.findOne({
@@ -27,33 +29,16 @@ export class UsersService {
 
   async create(createUserDto: CreateUserDto) {
     await this.validateCreateUserDto(createUserDto);
-    return this.userRepository.save({
+    return super.create({
       ...createUserDto,
       password: await bcrypt.hash(createUserDto.password, 10),
     });
   }
-
-  async findAll() {
-    return await this.userRepository.find();
-  }
-
-  async findOne(id: number) {
-    return await this.userRepository.findOneBy({ id });
-  }
-
   async findByEmail(email: string) {
     const user = await this.userRepository.findOneBy({ email });
     if (!user) {
       return null;
     }
     return user;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  async remove(id: number) {
-    return await this.userRepository.delete(id);
   }
 }
