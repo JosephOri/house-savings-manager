d4e3c6ff7046fcd3e9093ad6e29d5f17a7e7376a
josephOri <ori100no@gmail.com>
2025-12-06 13:42:08 +0200

    moved users into auth

--- src/auth/users/users.module.ts
+++ src/auth/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@app/common';
 
 @Module({
-  controllers: [UsersController],
+  imports: [TypeOrmModule.forFeature([User])],
+  exports: [TypeOrmModule],
   providers: [UsersService],
 })
 export class UsersModule {}
